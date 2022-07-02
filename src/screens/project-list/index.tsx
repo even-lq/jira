8db17066141145b7f40792ef32602faf74b2db53
even-lq <lqnwd999@163.com>
2022-07-02 22:51:46 +0800

    chore: 添加ts类型定义

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { cleanObject, useDebounce, useMount } from "../../utils";
+import * as qs from "qs"
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([])
+  const [param, setParam] = useState({
+    name: '',
+    personId: ''
+  })
+  const debouncedParam = useDebounce(param, 2000);
+  const [list, setList] = useState([])
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
+      if (response.ok) {
+        setList(await response.json())
+      }
+    })
+  }, [debouncedParam])
+
+  // 只需要在页面渲染的时候触发一次
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async response => {
+      if (response.ok) {
+        setUsers(await response.json())
+      }
+    })
+  })
+
+  return <div>
+    <SearchPanel users={users} param={param} setParam={setParam}/>
+    <List users={users} list={list}/>
+  </div> 
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value: any) => value === 0 ? false : !value;
+
+export const cleanObject = (object: object) => {
+  // Object.assign({}, object)
+  const res = {...object}
+  Object.keys(res).forEach((key: string) => {
+    // @ts-ignore
+    const value = res[key]
+    if (isFalsy(value)) {
+      // @ts-ignore
+      delete res[key]
+    }
+  })
+  return res;
+}
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+export const useDebounce = (value: any, delay?: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  // param每次变化都会执行useEffect
+  // 设置一个定时器用于更新param的值，且每次都会清理上一个定时器
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay)
+    // useEffect清除函数的特性
+    // 所以下一个useEffect运行前会清除上一个useEffect，
+    // 从而清除上一个定时器
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [value, delay])
+  return debouncedValue
+}
