a6966030a32dd87cd5a34999cc40d1a7833dbd97
even-lq <lqnwd999@163.com>
2022-07-17 15:42:23 +0800

    feat:美化authenticated-app的样式

--- src/authenticatetd-app.tsx
+++ src/authenticatetd-app.tsx
@@ -1,11 +1,39 @@
+import styled from "@emotion/styled"
+import { Row } from "components/lib"
 import { useAuth } from "context/auth-context"
 import { useState } from "react"
 import { ProjectListScreen } from "screens/project-list"
 
 export const AuthenticatedApp = () => {
   const {logout} = useAuth()
-  return <div>
-    <button onClick={logout}>登出</button>
-    <ProjectListScreen />
-  </div>
+  return <Container>
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <h3>Logo</h3>
+        <h3>项目</h3>
+        <h3>用户</h3>
+      </HeaderLeft>
+      <HeaderRight>
+        <button onClick={logout}>登出</button>
+      </HeaderRight>
+    </Header>
+    <Main>
+      <ProjectListScreen />
+    </Main>
+  </Container>
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`
+
+const Header = styled(Row)`
+`;
+const HeaderLeft = styled(Row)`
+
+`;
+const HeaderRight = styled.div``;
+const Main = styled.main`
+`;
