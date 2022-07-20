e3b93eccfaaaa116a9157bc6e4809d300bdda8c6
even-lq <lqnwd999@163.com>
2022-07-20 14:40:08 +0800

    feat:处理me接口和error

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -3,6 +3,8 @@ import * as auth from 'auth-provider'
 import { User } from "screens/project-list/search-panel";
 import { http } from "utils/http";
 import { useMount } from "utils";
+import { useAsync } from "utils/use-async";
+import { FullPageErrorFallback, FullPageLoading } from "components/lib";
 
 // 初始化，用户刷新页面
 const bootstrapUser = async () => {
@@ -29,7 +31,7 @@ interface AuthForm {
 }
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null)
+  const {data:user, error, isLoading, isIdle, isError, run, setData:setUser} = useAsync<User | null>()
 
   // then(user => setUser(user))
   const login = (form: AuthForm) => auth.login(form).then(setUser)
@@ -37,8 +39,16 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const logout = () => auth.logout().then(() => setUser(null))
 
   useMount(() => {
-    bootstrapUser().then(setUser);
+    run(bootstrapUser())
   })
+
+  if (isIdle || isLoading) {
+    return <FullPageLoading />
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error}/>
+  }
 
   return <AuthContext.Provider children={children} value={{user, login, register, logout}} />
 }
