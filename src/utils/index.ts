553ac8975ca98c900d563b7167c1a1e770534fe0
even-lq <lqnwd999@163.com>
2022-07-29 00:07:12 +0800

    chore: 完善useDocumentTitle

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const isFalsy = (value: unknown) => value === 0 ? false : !value;
 
@@ -40,19 +40,26 @@ export const useDebounce = <V>(value: V, delay?: number) => {
 }
 
 export const useDocumentTitle = (title: string, keepOnUnmount: boolean = true) => {
-  const oldTitle = document.title;
+  const oldTitle = useRef(document.title).current;
+  // 页面加载时：oldTitle === 旧title Jira任务管理系统
+  // 加载后：oldTitle === 新title（即参数title）
 
   useEffect(() => {
     document.title = title;
   }, [title])
 
+
+  // 由于hook中闭包的关系，使用useEffect hook不指定依赖，
+  // 当页面加载时，useEffect执行，那么调用的值依然是初始值
+  // 解决方式为指定依赖，让hook闭包可以追踪到变化
   useEffect(() => {
 
     // 页面卸载的时候调用
     return () => {
       if (!keepOnUnmount) {
+        // 如果不指定依赖，读到的还是旧title
         document.title = oldTitle;
       }
     }
-  }, [])
+  }, [keepOnUnmount, oldTitle])
 }
