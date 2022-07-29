4262083337b61c5c4aa76cc842dd60dbb6d50f6b
even-lq <lqnwd999@163.com>
2022-07-29 14:53:56 +0800

    feat:添加projects和projectId路由

--- src/authenticatetd-app.tsx
+++ src/authenticatetd-app.tsx
@@ -1,35 +1,46 @@
 import styled from "@emotion/styled"
 import { Row } from "components/lib"
 import { useAuth } from "context/auth-context"
-import { useState } from "react"
 import { ProjectListScreen } from "screens/project-list"
+import { ProjectScreen } from "screens/project"
 import { ReactComponent as SoftwareLogo } from 'assets/software-logo.svg'
 import { Button, Dropdown, Menu } from "antd"
+import { Navigate, Route, Routes } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export const AuthenticatedApp = () => {
-  const { logout, user} = useAuth()
   return <Container>
-    <Header between={true}>
-      <HeaderLeft gap={true}>
-        <SoftwareLogo width={'18rem'} color={'rgb(38, 132, 255)'}/>
-        <h3>项目</h3>
-        <h3>用户</h3>
-      </HeaderLeft>
-      <HeaderRight>
-        <Dropdown overlay={<Menu>
-          <Menu.Item key={'logout'}>
-            <Button type={"link"} onClick={logout}>登出</Button>
-          </Menu.Item>
-        </Menu>}>
-          {/* 防止页面重新刷新 */}
-          <Button onClick={e => e.preventDefault()}>Hi, {user?.name}</Button>
-        </Dropdown>
-      </HeaderRight>
-    </Header>
+    <PageHeader />
     <Main>
-      <ProjectListScreen />
+      <Router>
+        <Routes>
+          <Route path={'/projects'} element={<ProjectListScreen />} />
+          <Route path={'/projects/:projectId/*'} element={<ProjectScreen />} />
+        </Routes>
+      </Router>
     </Main>
   </Container>
+}
+
+const PageHeader = () => {
+  const { logout, user } = useAuth()
+  return <Header between={true}>
+    <HeaderLeft gap={true}>
+      <SoftwareLogo width={'18rem'} color={'rgb(38, 132, 255)'} />
+      <h3>项目</h3>
+      <h3>用户</h3>
+    </HeaderLeft>
+    <HeaderRight>
+      <Dropdown overlay={<Menu>
+        <Menu.Item key={'logout'}>
+          <Button type={"link"} onClick={logout}>登出</Button>
+        </Menu.Item>
+      </Menu>}>
+        {/* 防止页面重新刷新 */}
+        <Button onClick={e => e.preventDefault()}>Hi, {user?.name}</Button>
+      </Dropdown>
+    </HeaderRight>
+  </Header>
 }
 
 const Container = styled.div`
--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,6 +1,7 @@
 import { User } from "screens/project-list/search-panel";
 import { Table, TableProps } from "antd";
 import dayjs from "dayjs";
+import { Link } from "react-router-dom";
 
 export interface Project {
   id: string;
@@ -18,8 +19,10 @@ export const List = ({ users, ...props }: ListProps) => {
   return <Table pagination={false} columns={[
     {
       title: '名称',
-      dataIndex: 'name',
-      sorter: (a, b) => a.name.localeCompare(b.name)
+      sorter: (a, b) => a.name.localeCompare(b.name),
+      render(value, project) {
+        return <Link to={String(project.id)}>{project.name}</Link>
+      }
     },
     {
       title: '部门',
